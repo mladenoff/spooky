d28518116829c62f4d94959b352454941d9e31fb
Isak Mladenoff <imladenoff@gmail.com>
2017-07-18 14:27:21 -0700

    Write session reducer

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,1 +1,26 @@
-import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS } from '../action/session_actions';
+import {
+  RECEIVE_CURRENT_USER,
+  RECEIVE_ERRORS
+} from '../action/session_actions';
+
+const defaultState = () => ({
+  currentUser: null,
+  errors: []
+});
+
+const SessionReducer = (state = defaultState(), action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      // return Object.assign({}, state, {currentUser: action.currentUser,
+      //   errors: []});
+      return {currentUser: action.currentUser, errors: []};
+    case RECEIVE_ERRORS:
+      // return state;
+      return {currentUser: null, errors: action.errors};
+    default:
+      return state;
+  }
+};
+
+export default SessionReducer;
