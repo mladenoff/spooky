c375dfc86d7da98a9f0625526301a80e05574392
Isak Mladenoff <imladenoff@gmail.com>
2018-04-04 06:45:16 -0700

    Reorganize search actions

--- frontend/actions/search_actions.js
+++ frontend/actions/search_actions.js
@@ -1,12 +1,13 @@
 import * as APIUtil from '../util/search_api_util';
+
 import { START_FETCHING } from '../reducers/fetching_reducer';
 import { RECEIVE_TRACKS } from './track_actions';
+
+export const CLEAR_SEARCH = 'CLEAR_SEARCH';
 
 export const startFetchingSearchResults = () => ({
   type: START_FETCHING,
 });
-
-export const CLEAR_SEARCH = 'CLEAR_SEARCH';
 
 export const receiveSearch = results => ({ // At this stage search results are always tracks
   type: RECEIVE_TRACKS,
@@ -19,7 +20,7 @@ export const clearSearch = () => ({ // At this stage search results are always t
 
 export const requestSearchResults = search => (dispatch) => {
   dispatch(startFetchingSearchResults());
-  return APIUtil.fetchSearchResults(search).then(
-    results => (dispatch(receiveSearch(results))), // At this stage search results are always tracks
-  );
+  return APIUtil.fetchSearchResults(search).then(results => (
+    dispatch(receiveSearch(results)) // At this stage search results are always tracks
+  ));
 };
